dfd527e32a41a6740023d037872e6ed69a83f3e6
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-21 17:55:08 +0900

    Update sample1.js

--- sample1.js
+++ sample1.js
@@ -15,17 +15,16 @@ var barChartData = {
    labels : ["白神フブキ","大神ミオ","戌神ころね","猫又おかゆ"],
    datasets : [
       {
-         fillColor : "rgba(240,128,128,0.6)",    // 塗りつぶし色
-         strokeColor : "rgba(240,128,128,0.9)",  // 枠線の色
-         highlightFill: "rgba(255,64,64,0.75)",  // マウスが載った際の塗りつぶし色
-         highlightStroke: "rgba(255,64,64,1)",   // マウスが載った際の枠線の色
-         data : [ 40, 30, 40, 45]     // 各棒の値(カンマ区切りで指定)
+         fillColor : "rgba(240,128,128,0.6)",   
+         strokeColor : "rgba(240,128,128,0.9)",  
+         highlightFill: "rgba(255,64,64,0.75)",  
+         highlightStroke: "rgba(255,64,64,1)",   
+         data : [ 40, 30, 40, 45]     
       }
    ]
 
 }
 
-// ▼上記のグラフを描画するための記述
 window.onload = function(){
    var ctx = document.getElementById("graph-area").getContext("2d");
    window.myBar = new Chart(ctx).Bar(barChartData);
