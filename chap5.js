e685d568c7d569476b8efa78dec68746c1e83a04
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-21 16:19:26 +0900

    Create chap5.js

---
+++ chap5.js
@@ -0,0 +1,18 @@
+  $('.list-menu').on('click',function(){
+      $(this).css('color', '#ff0000');
+  });
+   $('#slideL').on('click',function(){
+     if($('#slideL').hasClass('off')){
+       $('#slideL').removeClass('off fa-angle-double-right');
+       $('#slideL').addClass('fa-angle-double-left');
+       $('.slide-close').animate({
+         'left': '0px'
+       },300);
+     } else {
+       $('#slideL').addClass('off fa-angle-double-right');
+       $('#slideL').removeClass('fa-angle-double-left');
+       $('.slide-close').animate({
+         'left': '-100px'
+       },300);
+     }
+   });
