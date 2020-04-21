e4d285f1b9883717e4d1bd07c5bb2ab767c2af97
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-21 17:56:59 +0900

    Update sample1.js

--- sample1.js
+++ sample1.js
@@ -15,11 +15,11 @@ var barChartData = {
    labels : ["白神フブキ","大神ミオ","戌神ころね","猫又おかゆ"],
    datasets : [
       {
-         fillColor : "rgba(240,128,128,0.6)",   
-         strokeColor : "rgba(240,128,128,0.9)",  
+         fillColor : "rgba(240,128,128,0.6)",
+         strokeColor : "rgba(240,128,128,0.9)",
          highlightFill: "rgba(255,64,64,0.75)",  
-         highlightStroke: "rgba(255,64,64,1)",   
-         data : [ 40, 30, 40, 45]     
+         highlightStroke: "rgba(255,64,64,1)",  
+         data : [ 40, 30, 40, 45]
       }
    ]
 
