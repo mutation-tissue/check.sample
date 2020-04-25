9ee4f2461a3e48050d9d0ab032b8c78a2ea4b4d7
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-25 21:50:18 +0900

    Update sample1.js

--- sample1.js
+++ sample1.js
@@ -1,42 +1,41 @@
+
 let slideshow = ()=> {
   document.getElementById('el').classList.toggle('fas');
   document.getElementById('el').classList.toggle('far');
-}
-let myfunc = () => {
-  alert("制作者名");
+  alert("fa");
 }
 let check = () => {
   alert("クリックされました");
 }
 let sidemenu = () => {
-  alert("表示しました")
+  alert("表示しました");
+}
+let clear = () => {
+  alert("消去しました");
 }
 var count1 = load_number();
 var barChartData = {
    labels : ["白神フブキ","大神ミオ","戌神ころね","猫又おかゆ"],
    datasets : [
       {
-         fillColor : "rgba(240,128,128,0.6)",
-         strokeColor : "rgba(240,128,128,0.9)",
-         highlightFill: "rgba(255,64,64,0.75)",  
-         highlightStroke: "rgba(255,64,64,1)",  
-         data : [ count1, 30, 40, 45]
+         fillColor : "rgba(240,128,128,0.6)",    // 塗りつぶし色
+         strokeColor : "rgba(240,128,128,0.9)",  // 枠線の色
+         highlightFill: "rgba(255,64,64,0.75)",  // マウスが載った際の塗りつぶし色
+         highlightStroke: "rgba(255,64,64,1)",   // マウスが載った際の枠線の色
+         data : [ count1, 30, 40, 45]     // 各棒の値(カンマ区切りで指定)
       }
    ]
 
 }
 
+// ▼上記のグラフを描画するための記述
 window.onload = function(){
+  var count = load_number();
    var ctx = document.getElementById("graph-area").getContext("2d");
    window.myBar = new Chart(ctx).Bar(barChartData);
-  var count1 = load_number();
+// ページが読み込まれたら実行
+var count1 = load_number();
 document.getElementById("dayCount").innerHTML = count1;
-};
-let reset = ()=> {
-  var count1 = load_number();
-  count1 = 0;
-  save(count1);
-  document.getElementById("dayCount").innerHTML = count1;
 }
 let reset = ()=> {
   var count1 = load_number();
