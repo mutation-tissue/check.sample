1a159b5581b121fdf45cf692699f833cdeb86a2b
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-25 21:47:15 +0900

    Update sample1.js

--- sample1.js
+++ sample1.js
@@ -11,6 +11,7 @@ let check = () => {
 let sidemenu = () => {
   alert("表示しました")
 }
+var count1 = load_number();
 var barChartData = {
    labels : ["白神フブキ","大神ミオ","戌神ころね","猫又おかゆ"],
    datasets : [
@@ -19,7 +20,7 @@ var barChartData = {
          strokeColor : "rgba(240,128,128,0.9)",
          highlightFill: "rgba(255,64,64,0.75)",  
          highlightStroke: "rgba(255,64,64,1)",  
-         data : [ 40, 30, 40, 45]
+         data : [ count1, 30, 40, 45]
       }
    ]
 
@@ -28,4 +29,40 @@ var barChartData = {
 window.onload = function(){
    var ctx = document.getElementById("graph-area").getContext("2d");
    window.myBar = new Chart(ctx).Bar(barChartData);
+  var count1 = load_number();
+document.getElementById("dayCount").innerHTML = count1;
+};
+let reset = ()=> {
+  var count1 = load_number();
+  count1 = 0;
+  save(count1);
+  document.getElementById("dayCount").innerHTML = count1;
+}
+let reset = ()=> {
+  var count1 = load_number();
+  count1 = 0;
+  save(count1);
+  document.getElementById("dayCount").innerHTML = count1;
+}
+let countup = ()=>{
+  var count1 = load_number();
+  count1 += 1;
+  save(count1);
+  document.getElementById("dayCount").innerHTML = count1;
+}
+function load_number() {
+    var count_data = localStorage.getItem('count1');
+    var count = Number(count_data);
+    if(
+      count &&
+      1 <= count &&
+      Math.floor(count) === count
+    ) {
+      return count;
+    } else {
+      return 0;
+    }
+}
+function save (count) {
+    localStorage.setItem("count1",count);
 }
