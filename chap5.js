5a2b35214983ef9e5f84fcfa5c7de9725e7852a0
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-28 10:00:19 +0900

    Update chap5.js

--- chap5.js
+++ chap5.js
@@ -1,17 +1,17 @@
-  jQuery('.list-menu').on('click',function(){
-      jQuery(this).css('color', '#ff0000');
+  $('.list-menu').on('click',function(){
+      $(this).css('color', '#ff0000');
   });
-   jQuery('#slideL').on('click',function(){
-     if(jQuery('#slideL').hasClass('off')){
-       jQuery('#slideL').removeClass('off fa-angle-double-right');
-       jQuery('#slideL').addClass('fa-angle-double-left');
-       jQuery('.slide-close').animate({
+   $('#slideL').on('click',function(){
+     if($('#slideL').hasClass('off')){
+       $('#slideL').removeClass('off fa-angle-double-right');
+       $('#slideL').addClass('fa-angle-double-left');
+       $('.slide-close').animate({
          'left': '0px'
        },300);
      } else {
-       jQuery('#slideL').addClass('off fa-angle-double-right');
-       jQuery('#slideL').removeClass('fa-angle-double-left');
-       jQuery('.slide-close').animate({
+       $('#slideL').addClass('off fa-angle-double-right');
+       $('#slideL').removeClass('fa-angle-double-left');
+       $('.slide-close').animate({
          'left': '-150px'
        },300);
      }
