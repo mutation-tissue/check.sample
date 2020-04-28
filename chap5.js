f74cd7732c106f26db7e90a0607f637d98a9ad01
mutation-tissue <61775717+mutation-tissue@users.noreply.github.com>
2020-04-28 09:58:24 +0900

    Update chap5.js

--- chap5.js
+++ chap5.js
@@ -1,17 +1,17 @@
-  $('.list-menu').on('click',function(){
+  jQuery('.list-menu').on('click',function(){
       $(this).css('color', '#ff0000');
   });
-   $('#slideL').on('click',function(){
+   jQuery('#slideL').on('click',function(){
      if($('#slideL').hasClass('off')){
-       $('#slideL').removeClass('off fa-angle-double-right');
-       $('#slideL').addClass('fa-angle-double-left');
-       $('.slide-close').animate({
+       jQuery('#slideL').removeClass('off fa-angle-double-right');
+       jQuery('#slideL').addClass('fa-angle-double-left');
+       jQuery('.slide-close').animate({
          'left': '0px'
        },300);
      } else {
-       $('#slideL').addClass('off fa-angle-double-right');
-       $('#slideL').removeClass('fa-angle-double-left');
-       $('.slide-close').animate({
+       jQuery('#slideL').addClass('off fa-angle-double-right');
+       jQuery('#slideL').removeClass('fa-angle-double-left');
+       jQuery('.slide-close').animate({
          'left': '-150px'
        },300);
      }
